c5738d7f160584d760154eb3e4cc729a26d3257b
josh <82616603+joshm3@users.noreply.github.com>
2024-02-16 16:41:54 -0500

    Update downloadHashMob.sh

--- downloadHashMob.sh
+++ downloadHashMob.sh
@@ -4,11 +4,11 @@
 #These datasets will change overtime as more people crack passwords and upload them
 #Thus all research should be careful to compare on the same dataset versions
 
-wget -O minecraft.csv https://cdn.hashmob.net/hashlists/466/466.2811.found #minecraft 149,000
-# wget -O mangatraders.csv https://cdn.hashmob.net/hashlists/752/752.0.found #mangatraders 619,000
-# wget -O wattpad.csv https://cdn.hashmob.net/hashlists/4364/4364.3200.found #wattpad 23,000,000
-# wget -O battlefield.csv https://cdn.hashmob.net/hashlists/541/541.0.found #battlefield 420,000
-# wget -O wanelo.csv https://cdn.hashmob.net/hashlists/2925/2925.0.found #wanelo 2,000,000
-# wget -O everydayrecipes.csv https://cdn.hashmob.net/hashlists/134/134.0.found #everydayrecipes 25,000
-# wget -O zynga.csv https://cdn.hashmob.net/hashlists/740/740.27200.found #zynga 48,000,000
-# wget -O dosportseasy.csv https://cdn.hashmob.net/hashlists/268/268.100.found #dosportseasy 45,000
+wget -O minecraft.found https://cdn.hashmob.net/hashlists/466/466.2811.found #minecraft 149,000
+# wget -O mangatraders.found https://cdn.hashmob.net/hashlists/752/752.0.found #mangatraders 619,000
+# wget -O wattpad.found https://cdn.hashmob.net/hashlists/4364/4364.3200.found #wattpad 23,000,000
+# wget -O battlefield.found https://cdn.hashmob.net/hashlists/541/541.0.found #battlefield 420,000
+# wget -O wanelo.found https://cdn.hashmob.net/hashlists/2925/2925.0.found #wanelo 2,000,000
+# wget -O everydayrecipes.found https://cdn.hashmob.net/hashlists/134/134.0.found #everydayrecipes 25,000
+# wget -O zynga.found https://cdn.hashmob.net/hashlists/740/740.27200.found #zynga 48,000,000
+# wget -O dosportseasy.found https://cdn.hashmob.net/hashlists/268/268.100.found #dosportseasy 45,000
